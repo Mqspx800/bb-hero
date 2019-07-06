80697f426d2679f4a9a764092ecb791bb0f9904e
Mqspx800 <applefor1988@gmail.com>
2019-07-07 01:05:34 +0200

    final

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -2,24 +2,22 @@
 
 //initialize Hero
 var hero;
-var init = false;
-var brickRowCount
-var brickColumnCount
-var brickWidth
-var brickHeight
-var brickPadding 
-var brickOffsetTop 
-var brickOffsetLeft 
-var bricks 
-var imgHero
-var rightPressed = false
-var leftPressed = false
-var upPressed = false
-var downPressed = false
+var rightPressed
+var leftPressed
+var upPressed
+var downPressed
+var monster
+var weapon
 function initializeGame(){
-    if(!hero)
-    hero = new Object;
+   
+     hero = new Object;
+     monster = new Object
+     weapon = new Object
+     document.addEventListener("keydown", keyDownHandler, false);
+    
     //console.log('game load')
+    
+    hero.imgHero = document.getElementById('heroIMG')
     hero.name = '';
     hero.heroic = false;
     hero.inventory = [];
@@ -27,10 +25,14 @@ function initializeGame(){
     hero.weapon = {type:'',damage:2}
     hero.currentIndex = [0,0]
     
-  
-var inn = document.getElementById('inn').onclick = function(){rest(hero)}
-var dagger = document.getElementById('dagger').onclick=
-function(){pickUpItem(hero,{type:'dagger',damage:2})}
+    rightPressed = false
+    leftPressed = false
+    upPressed = false
+    downPressed = false
+    var inn = document.getElementById('inn').onclick = function(){rest(hero)}
+    var dagger = document.getElementById('dagger').onclick=
+        function(){pickUpItem(hero,{type:'dagger',damage:2})}
+    var bag = document.getElementById('bag').onclick = function(){equipWeapon(hero)}
     brickRowCount = 5;
     brickColumnCount = 5;
     brickWidth = 45;
@@ -39,29 +41,41 @@ function(){pickUpItem(hero,{type:'dagger',damage:2})}
     brickOffsetTop = 30;
     brickOffsetLeft = 30;
     bricks = [];
-    imgHero = document.createElement('img')
-    imgHero.id = 'hero'
-    imgHero.src = 'https://data.whicdn.com/images/301966215/original.gif'
-    imgHero.width = brickWidth
-    imgHero.height = brickHeight
-    renderBrick()
-    renderHero(0,2)
-   
-    init = true
+    monster.monsterIndex = generateRandomIndex()
+    monster.health = 10;
+    weapon.index = generateRandomIndex()
+    
 }
 
-function draw(){
-    initializeGame()
-    console.log(rightPressed)
-    if(rightPressed||leftPressed||upPressed||downPressed)
-    {
-        console.log('run??????')
-        moveHero()
-       
+function disposeWeapon()
+{
+    weapon.elementH = document.getElementById("dagger")
+     ctx.drawImage(weapon.elementH, bricks[weapon.index[0]][weapon.index[1]].x,bricks[weapon.index[0]][weapon.index[1]].y,brickWidth,brickHeight);
+     bricks[weapon.index[0]][weapon.index[1]].occupied = "weapon"
+}
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+        console.log('keyPress')
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
     }
 }
 
-var bag = document.getElementById('bag').onclick = function(){equipWeapon(hero)}
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+canvas.style.minWidth = 1100
+canvas.style.maxWidth = window.innerWidth
+canvas.style.height = 500
+canvas.style.display = 'none'
 
 function rest(target)
 {
@@ -74,22 +88,104 @@ function rest(target)
 
 function pickUpItem(hero,weapon)
 {
+    
     hero.inventory.push(weapon)
+    document.getElementById('states').value+=('pick up '+ weapon.type + 'with damage '+ weapon.damage +'\\\n');
+    console.log(hero.weapon.damage)
 }
 
 function equipWeapon(hero)
 {
     if(hero.inventory[0]!=undefined)
     hero.weapon= hero.inventory[0];
+    console.log(hero.weapon.damage)
 }
 
+function fight()
+{
+    var userChoice = prompt("Do you choose rock, paper or scissors?");
+    console.log(hero)
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+	computerChoice = "rock";
+  } else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+  } else {
+	computerChoice = "scissors";
+  } 
 
+    var compare = function(choice1,choice2) {
+    
+    if (choice1 === choice2) {
+        
+        return "The result is a tie!" + " " + "Nothing happen." ;
+    }
+    
+    else if (choice1 === "rock") {
+        
+        if (choice2 === "scissors") {
+            monster.health -= hero.weapon.damage
+            return "rock wins" + "\\\n" + hero.name+ " deal "+ hero.weapon.damage + " point damage to the monster";
+        }
+        else {
+            hero.health -= 5
+            return "paper wins" + "\\\n"+ hero.name + "received 5 damage from enemy";
+        }
+    }
+    
+    else if (choice1 === "paper") {
+        
+        if (choice2 === "rock") {
+            monster.health -= hero.weapon.damage
+            return "paper wins" + "\\\n"+ hero.name + "deal "+ hero.weapon.damage + " point damage  to the monster";
+        }
+        else {
+            hero.health -= 5
+            return "scissors wins" + "\\\n"+ hero.name + "received 5 damage from enemy";
+        }
+            
+    }
+    
+    else if (choice1 === "scissors") {
+        
+        if (choice2 === "rock") {
+            hero.health -= 5
+            return "rock wins" + "\\\n"+ hero.name + "received 5 damage from enemy";
+        }
+        else {
+            monster.health -= hero.weapon.damage
+            return "scissors win" + "\\\n"+ hero.name + "deal "+ hero.weapon.damage + " point damage to the monster";
+        }
+    }
+    
+    
+    } //closes compare function
+
+    if(hero.health > 0 )
+    {
+        document.getElementById('states').value+=("Computer chose: " + computerChoice+'\\\n');
+        document.getElementById('states').value+=(compare(userChoice,computerChoice)+'\\\n');
+        document.getElementById('states').value+=(hero.name + " 's health: "+hero.health+ "|| moster 's health:" + monster.health +"\\\n");
+    }
+    else if(monster.health<=0)
+    {
+        document.getElementById('states').value+='you win!';
+        alert("you win, press enter to restart!")
+        location.reload()
+    }
+    else if(hero.health<=0)
+    {
+        document.getElementById('states').value+='game over!';
+        alert("you lose, press enter to restart!")
+        location.reload()
+    }
+}
 
 function changeHeroName()
 {
     name = document.getElementById('hero_name').value
     //console.log(name)
-    if(name)
+    if(name!='')
     hero.name = this.name
     //console.log(hero.name)
     form = document.getElementById('myForm')
@@ -97,15 +193,21 @@ function changeHeroName()
     canvas.style.display = 'block'
 }
 
-//game logic
-var canvas = document.getElementById("myCanvas");
-//console.log(canvas)
-var ctx = canvas.getContext("2d");
-canvas.style.minWidth = 1100
-canvas.style.maxWidth = window.innerWidth
-canvas.style.height = 500
-canvas.style.display = 'none'
+function drawHero(grid_c,grid_r){
+    //console.log(hero.imgHero,bricks[grid_c][grid_r].x,bricks[grid_c][grid_r].y )
+    ctx.drawImage(hero.imgHero, bricks[grid_c][grid_r].x,bricks[grid_c][grid_r].y,brickWidth,brickHeight);
+    //hero.imgHero.src = 'lib/hero.gif'
+}
 
+
+function disposeMonster()
+{
+    
+     monster.elementH = document.getElementById("monster_pic")
+     //console.log(monster.monsterIndex);
+     ctx.drawImage(monster.elementH, bricks[monster.monsterIndex[0]][monster.monsterIndex[1]].x,bricks[monster.monsterIndex[0]][monster.monsterIndex[1]].y,brickWidth,brickHeight);
+     bricks[monster.monsterIndex[0]][monster.monsterIndex[1]].occupied = "monster"
+}
 
 
 // setup grid of playground
@@ -129,57 +231,82 @@ function renderBrick(){
             ctx.fill();
             ctx.closePath();
         }
-       // console.log(bricks)
     }
 }
 
+function draw()
+{
+    renderBrick()
+    disposeMonster()
+    disposeWeapon()
+    drawHero(hero.currentIndex[0],hero.currentIndex[1])
+    if(rightPressed && hero.currentIndex[0]<4 && bricks[hero.currentIndex[0]+1][hero.currentIndex[1]].occupied==null){
+        drawHero(hero.currentIndex[0]+1,hero.currentIndex[1])
+       
+        hero.currentIndex[0]++
+        
+    }
+    else if(leftPressed && hero.currentIndex[0]>0 && bricks[hero.currentIndex[0]-1][hero.currentIndex[1]].occupied==null){
+        drawHero(hero.currentIndex[0]-1,hero.currentIndex[1])
+      
+        hero.currentIndex[0]--
+        
+    }
+    else if(downPressed && hero.currentIndex[1]<4 && bricks[hero.currentIndex[0]][hero.currentIndex[1]+1].occupied==null){
+        drawHero(hero.currentIndex[0],hero.currentIndex[1]+1)
+ 
+        hero.currentIndex[1]++
+        
+    }
+    else if(upPressed && hero.currentIndex[1]>0 && bricks[hero.currentIndex[0]][hero.currentIndex[1]-1].occupied==null){
+        drawHero(hero.currentIndex[0],hero.currentIndex[1]-1)
+     
+        hero.currentIndex[1]--
+    }
+    checkWhatsInfront()
+    rightPressed = false;upPressed = false;downPressed = false;leftPressed = false;
+}
 
-function renderHero(ind_c,ind_r)
+function checkWhatsInfront()
+{
+    var target
+    if(rightPressed&&hero.currentIndex[0]+1<=4){
+        target = bricks[hero.currentIndex[0]+1][hero.currentIndex[1]].occupied
+    }else if(leftPressed&&hero.currentIndex[0]-1>=0)
     {
-       //console.log(bricks[ind_c][ind_r].x+ '  '+bricks[ind_c][ind_r].y)
-        ctx.drawImage(imgHero, bricks[ind_c][ind_r].x, bricks[ind_c][ind_r].y, imgHero.width, imgHero.height);
-        hero.currentIndex = [ind_c,ind_r]
-        //console.log(ind_c,ind_r)
+        target =bricks[hero.currentIndex[0]-1][hero.currentIndex[1]].occupied
+    }else if(upPressed&&hero.currentIndex[1]-1>=0){
+        target= bricks[hero.currentIndex[0]][hero.currentIndex[1]-1].occupied
+    }
+    else if(downPressed&&hero.currentIndex[1]+1<=4)
+    {
+        target = bricks[hero.currentIndex[0]][hero.currentIndex[1]+1].occupied
     }
 
 
-  
-    document.addEventListener("keydown", keyDownHandler, false);
+     if(target=='monster')
+     {
+         fight()
+     }
+     else if(target=='weapon'){
+        var w = {type:'dagger',damage:5}
+        pickUpItem(hero,w)
+     }
     
-    function keyDownHandler(e) {
-        if(e.key == "Right" || e.key == "ArrowRight") {
-            console.log('rightPress')
-            rightPressed = true;
-        }
-        else if(e.key == "Left" || e.key == "ArrowLeft") {
-            leftPressed = true;
-        }
-        else if(e.key == "up" || e.key == "ArrowUp") {
-            upPressed = true;
-        }else if(e.key == "down" || e.key == "ArrowDown") {
-            downPressed = true;
-        }
-    }
+    
+}
 
 
-    function moveHero(){
-        var heroC = hero.currentIndex[0]
-        var heroR = hero.currentIndex[1]
-        if(rightPressed && heroC<3 && bricks[heroC+1][heroR].occupied== null ) {
-            renderBrick();
-            renderHero([heroC+1][heroR])
-        }
-        else if(left_press && heroC>1 && bricks[heroC-1][heroR].occupied== null ) {
-            renderBrick()
-            renderHero([heroC-1][heroR])
-        }
-        else if(up_Pressed && heroC<3 && bricks[heroC][heroR+1].occupied== null ) {
-            renderBrick()
-            renderHero([heroC][heroR+1])
-        }
-        else if(down_Pressed && heroC>1 && bricks[heroC][heroR-1].occupied== null ) {
-            renderBrick()
-            renderHero([heroC-1][heroR])
-        }
-    }
-    setInterval(draw, 100);
+function generateRandomIndex()
+{
+    var i = [0,0]
+    var min=0; 
+    var max=4;  
+    
+    i[0] = parseInt(Math.random() * (+max - +min) + +min); 
+    i[1] = parseInt(Math.random() * (+max - +min) + +min); 
+
+    return i;
+}
+
+setInterval(draw, 100);
